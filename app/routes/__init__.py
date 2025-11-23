9dc7d179ea683f545118c514baf68c1e2bccc878
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-11-23 23:35:17 +0800

    Update __init__.py

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,5 +1,19 @@
-form .BMS_auth import auth
+from .BMS_main import main
+from .BMS_auth import auth
+from .BMS_admin import admin
+from .BMS_tools import tools
+from .BMS_filemanager import filem
+from .BMS_media_mp3 import mp3
+from .BMS_media_video import video
+from .BMS_user import user
 
 __all__ = [
+    "main",
     "auth",
+    "admin",
+    "tools",
+    "filem",
+    "mp3",
+    "video",
+    "user"
 ]
