6c57ce52d4e55aeb5aeb42683901526372ec7b73
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-11-24 01:40:19 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,29 @@
 from flask import Flask
-from app.routes import main, auth, admin, tools, filem, mp3, video, user
-
+from app.database.BMS_db import BMS_db_init
 
 def create_app():
     app = Flask(__name__)
+    app.secret_key = "BMS_SECRET_KEY"
 
-    # Registrasi blueprint khusus BMS
+    # ==================================================
+    #  INISIALISASI DATABASE (WAJIB!)
+    # ==================================================
+    BMS_db_init()
+
+    # ==================================================
+    #  REGISTER BLUEPRINT
+    # ==================================================
+    from app.routes import (
+        main,
+        auth,
+        admin,
+        tools,
+        filem,
+        mp3,
+        video,
+        user
+    )
+
     app.register_blueprint(main)
     app.register_blueprint(auth)
     app.register_blueprint(admin)
