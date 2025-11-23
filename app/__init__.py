dd150989324c9b7c6e8a4843af1a43ef388b4bae
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-11-23 20:09:18 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,8 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    # Registrasi blueprint
-    from .routes.main import main
+    # Registrasi blueprint khusus BMS
+    from .routes.BMS_main import main
     app.register_blueprint(main)
 
     return app
