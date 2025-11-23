19e982ed72d3b6d55f2b004092103906e0006d31
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-11-23 23:38:23 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from app.routes import main, auth, admin, tools, filem, mp3, video, user
+
 
 def create_app():
     app = Flask(__name__)
 
     # Registrasi blueprint khusus BMS
-    from .routes.BMS_main import main
     app.register_blueprint(main)
+    app.register_blueprint(auth)
+    app.register_blueprint(admin)
+    app.register_blueprint(tools)
+    app.register_blueprint(filem)
+    app.register_blueprint(mp3)
+    app.register_blueprint(video)
+    app.register_blueprint(user)
 
     return app
