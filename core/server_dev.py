603fcdb53c6d0c51d0a798305a799cf2bcb2e927
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-12-03 19:29:14 +0800

    Create server_dev.py

---
+++ core/server_dev.py
@@ -0,0 +1,17 @@
+# core/server_dev.py
+from core.system_tools import run
+
+def run_development(env: dict, venv_python: str):
+    """
+    Jalankan server development:
+    - Linux + gunicorn -> gunakan gunicorn
+    - Lainnya -> waitress
+    """
+    print("=== DEVELOPMENT MODE ===")
+    if env.get("os") == "linux" and env.get("has_gunicorn"):
+        cmd = f"{venv_python} -m gunicorn -w 2 --threads 2 -b 0.0.0.0:5000 BMS:create_app()"
+        print("[i] Menjalankan Gunicorn (development).")
+    else:
+        cmd = f"{venv_python} -m waitress --listen=0.0.0.0:5000 BMS:create_app"
+        print("[i] Menjalankan Waitress (development fallback).")
+    run(cmd)
