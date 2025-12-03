c14b27de7aa7c8cd10958889eb0fab15b1adfa5c
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-12-03 19:30:44 +0800

    Create server_prod.py

---
+++ core/server_prod.py
@@ -0,0 +1,16 @@
+# core/server_prod.py
+from core.nginx_tools import generate_nginx_config, reload_nginx
+from core.system_tools import run
+
+def run_production(env: dict, venv_python: str, project_dir: str):
+    print("=== PRODUCTION MODE ===")
+    if env.get("os") == "linux":
+        print("[i] Menyiapkan NGINX untuk production...")
+        generate_nginx_config(project_dir)
+        reload_nginx()
+        cmd = f"{venv_python} -m gunicorn -w 3 --threads 3 -b 127.0.0.1:5000 BMS:create_app()"
+        print("[i] Menjalankan Gunicorn (production).")
+    else:
+        cmd = f"{venv_python} -m waitress --listen=0.0.0.0:5000 BMS:create_app"
+        print("[i] Menjalankan Waitress (production fallback).")
+    run(cmd)
