53dcdca74016a7a07e487a45536437a06357dc3f
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-12-10 21:36:55 +0800

    Create __init__.py

---
+++ app/routes/BMS_mp3/__init__.py
@@ -0,0 +1,15 @@
+# ============================================================================
+#   BMS MP3 MODULE — BLUEPRINT INITIALIZER
+#   Menggabungkan:
+#       ✔ scan blueprint
+#       ✔ main mp3 routes
+# ============================================================================
+
+from .BMS_mp3_routes import media_mp3
+from .BMS_mp3_scan import mp3_scan
+
+# daftar blueprint untuk di-import oleh register_blueprints()
+blueprints = [
+    media_mp3,
+    mp3_scan
+]
