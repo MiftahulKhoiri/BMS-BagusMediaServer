7223ae1d88bd1cf88d06bc15b819d200c97f8d77
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-12-11 12:34:17 +0800

    Update BMS_video_routes.py

--- app/routes/BMS_video/BMS_video_routes.py
+++ app/routes/BMS_video/BMS_video_routes.py
@@ -17,7 +17,7 @@ video_routes = Blueprint("video_routes", __name__, url_prefix="/video")
 # ============================================================================
 #   HALAMAN VIDEO
 # ============================================================================
-@video_video.route("/")
+@video_routes.route("/")
 def BMS_video_page():
     if not BMS_auth_is_login():
         return render_template("BMS_login.html", error="Silakan login terlebih dahulu!")
