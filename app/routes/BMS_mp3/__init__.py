9b1beb1aa0365b5306b6ebe20e4a6787bf8c3131
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-12-23 12:59:04 +0800

    Update __init__.py

--- app/routes/BMS_mp3/__init__.py
+++ app/routes/BMS_mp3/__init__.py
@@ -7,6 +7,7 @@
 
 from .BMS_mp3_routes import media_mp3
 from .BMS_mp3_scan import mp3_scan
+app.register_blueprint(mp3_thumb)
 
 # daftar blueprint untuk di-import oleh register_blueprints()
 blueprints = [
