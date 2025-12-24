8b3686801eae96a5514f3f83a32968b7685699c0
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-12-24 20:21:17 +0800

    Update audio.py

--- app/routes/BMS_downlod/audio.py
+++ app/routes/BMS_downlod/audio.py
@@ -1,15 +1,25 @@
 from yt_dlp import YoutubeDL
 from app.routes.BMS_downlod.progress import yt_progress_hook
+from app.BMS_config import BASE_DOWNLOADS_FOLDER
+import os
 
-def download_audio(url):
+def download_mp3(url, nama_file):
+    # 📁 pastikan folder mp3 ada
+    mp3_folder = os.path.join(BASE_DOWNLOADS_FOLDER, "mp3")
+    os.makedirs(mp3_folder, exist_ok=True)
+
     opsi = {
-        "format": "bestaudio[ext=m4a]/bestaudio",
-        "outtmpl": "temp_audio.%(ext)s",
+        "format": "bestaudio",
+        "outtmpl": os.path.join(mp3_folder, f"{nama_file}.%(ext)s"),
         "noplaylist": True,
         "quiet": True,
         "progress_hooks": [yt_progress_hook],
+        "postprocessors": [{
+            "key": "FFmpegExtractAudio",
+            "preferredcodec": "mp3",
+            "preferredquality": "192",
+        }],
     }
 
     with YoutubeDL(opsi) as ydl:
-        info = ydl.extract_info(url, download=True)
-        return f"temp_audio.{info.get('ext','m4a')}"
+        ydl.extract_info(url, download=True)
