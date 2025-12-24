0d2dd6b525bfa284879fc362bb6c36bb2f94958e
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-12-24 21:59:54 +0800

    Update audio.py

--- app/routes/BMS_downlod/audio.py
+++ app/routes/BMS_downlod/audio.py
@@ -1,11 +1,11 @@
 from yt_dlp import YoutubeDL
 from app.routes.BMS_downlod.progress import yt_progress_hook
-from app.BMS_config import BASE_DOWNLOADS_FOLDER
+from app.BMS_config import DOWNLOADS_FOLDER
 from app.routes.BMS_downlod.file_helper import buat_nama_unik
 import os
 
 def download_mp3(url, title):
-    mp3_folder = os.path.join(BASE_DOWNLOADS_FOLDER, "mp3")
+    mp3_folder = os.path.join(DOWNLOADS_FOLDER, "mp3")
     os.makedirs(mp3_folder, exist_ok=True)
 
     nama_file = buat_nama_unik(mp3_folder, title, "mp3")
