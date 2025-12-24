f6099ebcf418ee58b68079832b40d9fd0d80cdbd
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-12-24 20:26:29 +0800

    Update file_helper.py

--- app/routes/BMS_downlod/file_helper.py
+++ app/routes/BMS_downlod/file_helper.py
@@ -11,3 +11,14 @@ def hapus_jika_ada(*files):
     for f in files:
         if f and os.path.exists(f):
             os.remove(f)
+
+def buat_nama_unik(folder, nama, ext):
+    base = nama
+    i = 1
+    hasil = f"{base}.{ext}"
+
+    while os.path.exists(os.path.join(folder, hasil)):
+        hasil = f"{base}_{i}.{ext}"
+        i += 1
+
+    return hasil
