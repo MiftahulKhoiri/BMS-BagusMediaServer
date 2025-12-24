3fe9240e43a8728f2986f64b4a49a58f0ac899d3
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-12-24 21:22:07 +0800

    Update progress.py

--- app/routes/BMS_downlod/progress.py
+++ app/routes/BMS_downlod/progress.py
@@ -1,7 +1,13 @@
-def yt_progress_hook(d):
-    if d["status"] == "downloading":
-        percent = d.get("_percent_str", "").strip()
-        speed = d.get("_speed_str", "").strip()
-        print(f"\r⬇️ {percent} | {speed}", end="")
-    elif d["status"] == "finished":
-        print("\n✅ Download selesai")
+from app.routes.BMS_downlod.progress_store import update_task
+
+def yt_progress_hook(task_id=None):
+    def hook(d):
+        if not task_id:
+            return
+
+        if d["status"] == "downloading":
+            percent = d.get("_percent_str", "").strip()
+            update_task(task_id, status="downloading", progress=percent)
+        elif d["status"] == "finished":
+            update_task(task_id, status="finished", progress="100%")
+    return hook
