3da8377771a96e4e90ff09b2772dd401d1778d57
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-12-24 21:59:25 +0800

    Update video.py

--- app/routes/BMS_downlod/video.py
+++ app/routes/BMS_downlod/video.py
@@ -1,6 +1,6 @@
 from yt_dlp import YoutubeDL
 from app.routes.BMS_downlod.progress import yt_progress_hook
-from app.BMS_config import BASE_DOWNLOADS_FOLDER
+from app.BMS_config import DOWNLOADS_FOLDER
 
 def unduh_video(url, resolusi=720):
     cek_ffmpeg()
@@ -8,7 +8,7 @@ def unduh_video(url, resolusi=720):
         return None
 
     # 📁 pastikan folder video ada
-    video_folder = os.path.join(BASE_DOWNLOADS_FOLDER, "video")
+    video_folder = os.path.join(DOWNLOADS_FOLDER, "video")
     os.makedirs(video_folder, exist_ok=True)
 
     title, video = download_video(url, resolusi)
