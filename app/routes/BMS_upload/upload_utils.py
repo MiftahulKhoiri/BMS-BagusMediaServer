6df33f7669adc5654e02a7de9dae22909bc922df
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-12-31 18:59:07 +0800

    Update upload_utils.py

--- app/routes/BMS_upload/upload_utils.py
+++ app/routes/BMS_upload/upload_utils.py
@@ -1,4 +1,15 @@
 import shutil
+import os
+
+def detect_category(filename: str) -> str:
+    ext = os.path.splitext(filename.lower())[1]
+
+    if ext in [".mp3", ".wav", ".flac"]:
+        return "media/mp3"
+    if ext in [".mp4", ".mkv", ".avi"]:
+        return "media/video"
+
+    return "media/other"
 
 def check_disk_space(required_size: int) -> bool:
     total, used, free = shutil.disk_usage("/")
