844f17c2666133611fb24fb276cc6a069150d34a
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-12-31 18:25:52 +0800

    Update upload_auth.py

--- app/routes/BMS_upload/upload_auth.py
+++ app/routes/BMS_upload/upload_auth.py
@@ -1,13 +1,22 @@
+from functools import wraps
 from flask import jsonify
+
 from app.routes.BMS_auth.session_helpers import (
     BMS_auth_is_login,
     BMS_auth_is_admin,
     BMS_auth_is_root
 )
 
-def fm_auth():
-    if not BMS_auth_is_login():
-        return jsonify({"error": "Belum login"}), 403
-    if not (BMS_auth_is_admin() or BMS_auth_is_root()):
-        return jsonify({"error": "Akses ditolak"}), 403
-    return None
+def fm_auth(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+
+        if not BMS_auth_is_login():
+            return jsonify({"error": "Belum login"}), 403
+
+        if not (BMS_auth_is_admin() or BMS_auth_is_root()):
+            return jsonify({"error": "Akses ditolak"}), 403
+
+        return func(*args, **kwargs)
+
+    return wrapper
